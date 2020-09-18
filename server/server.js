a3b4751e2acdb2e01b8f9a5def58ac7df7156e5c
Kartik Goel <goel.kartik39@gmail.com>
2020-09-19 02:43:26 +0530

    Setup basic server

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
+const db = process.env.MONGODB_URL;
+
+mongoose.connect(db, {useNewUrlParser : true,
+    useUnifiedTopology: true,
+    useCreateIndex : true})
+.then(() => console.log('Database connected'))
+.catch((err) => {
+    console.log(err);
+})
+
+app.use(express.urlencoded({
+    extended : false
+}));
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+});
