b006a07f8264c36b701c48558370cf5e90a61ee4
Kartik Goel <goel.kartik39@gmail.com>
2020-09-19 21:30:24 +0530

    Added schema

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const patientAuth = require("./routes/patientAuth");
 const doctorAuth = require("./routes/doctorAuth");
 const prescription = require("./routes/prescription");
 const appointment = require("./routes/appointment");
+const donor = require("./routes/donor");
 
 const db = process.env.MONGODB_URL;
 
@@ -36,6 +37,7 @@ app.use("/patient",patientAuth);
 app.use("/doctor",doctorAuth);
 app.use("/prescription",prescription);
 app.use("/appointment",appointment);
+app.use("/donor",donor);
 
 const PORT = process.env.PORT || 5000;
 
