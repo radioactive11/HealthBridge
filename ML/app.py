b84d6500771bfd4c282aae0b9c41fe536b033cd9
radiactive11 <roy.arijit2001@gmail.com>
2020-09-19 15:51:26 +0530

    Cancer-API

--- ML/app.py
+++ ML/app.py
@@ -2,13 +2,16 @@ from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from flask_cors import CORS, cross_origin
 from scripts.pneumonia.pneumonia_detection import run_predictions
+from scripts.cancer.cancer import predict_cancer
 import shutil
 from PIL import Image
 import os
 
+
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
 
 class PneumoniaDetection(Resource):
     def post(self):
@@ -27,6 +30,25 @@ class PneumoniaDetection(Resource):
 
 
 api.add_resource(PneumoniaDetection, "/pneumonia")
+
+
+class Cancer(Resource):
+    def post(self):
+        if os.path.exists("scripts/cancer/UPLOAD_IMG/"):
+            shutil.rmtree("scripts/cancer/UPLOAD_IMG/")
+        os.mkdir("scripts/cancer/UPLOAD_IMG")
+        os.mkdir("scripts/cancer/UPLOAD_IMG/image")
+        image = request.files["images"]
+        image_name = image.filename
+        PATH_UPLOAD = os.path.join(os.getcwd(), "scripts/cancer/UPLOAD_IMG/image")
+        image.save(os.path.join(PATH_UPLOAD, image_name))
+
+        result = predict_cancer("scripts/cancer/UPLOAD_IMG")
+        shutil.rmtree("scripts/cancer/UPLOAD_IMG")
+        return result[0]
+
+
+api.add_resource(Cancer, "/cancer")
 
 
 if __name__ == "__main__":
