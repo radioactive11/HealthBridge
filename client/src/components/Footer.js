5f1c2bc7441143f42f0559cf46cf0086b4294a05
Haqverdi <haqverdib@gmail.com>
2020-10-01 13:38:23 +0400

    add github star and fork buttons footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React from 'react';
+import GitHubButton from 'react-github-btn';
 
 const Footer = () => {
-	return (
-		<div className="Footer">
-			<div className="text">
-				Made with{" "}
-				<span role="img" aria-label="f" aria-labelledby="f">
-					💖
-				</span>{" "}
-				by <a href="/">Team Hack Overflow</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className="Footer">
+      <div className="text">
+        Made with{' '}
+        <span role="img" aria-label="f" aria-labelledby="f">
+          💖
+        </span>{' '}
+        by <a href="/">Team Hack Overflow</a>
+      </div>
+
+      <span className="margin-left">
+        <GitHubButton
+          href="https://github.com/radioactive11/HealthBridge"
+          data-icon="octicon-star"
+          data-size="large"
+          aria-label="Star radioactive11/HealthBridge on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </span>
+
+      <span className="margin-left text">or</span>
+
+      <span className="margin-left">
+        <GitHubButton
+          href="https://github.com/radioactive11/HealthBridge/fork"
+          data-icon="octicon-repo-forked"
+          data-size="large"
+          aria-label="Fork radioactive11/HealthBridge on GitHub"
+        >
+          Fork
+        </GitHubButton>
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
