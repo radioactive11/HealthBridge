86034666359b92d234a4a61df473d19e7e97185a
Ayush Khandelwal <ayush@Ayushs-MacBook-Air.local>
2020-10-11 20:13:55 +0530

    Dark mode settings persist even after refreshing the page

--- client/src/components/darktoggle.js
+++ client/src/components/darktoggle.js
@@ -1,25 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 import Toggle from 'react-toggle'
 import { BsMoon } from "react-icons/bs";
 import { BsSun } from "react-icons/bs";
 
-   const DARK_CLASS = "dark";
+const DARK_CLASS = "dark";
 
 const DarkToggle = () => {
-  const systemPrefersDark = useMediaQuery(
-    {
-      query: "(prefers-color-scheme: dark)"
-    },
-    undefined,
-    prefersDark => {
-      setIsDark(prefersDark);
-    }
-  );
+  // const systemPrefersDark = useMediaQuery(
+  //   {
+  //     query: "(prefers-color-scheme: dark)"
+  //   },
+  //   undefined,
+  //   prefersDark => {
+  //     setIsDark(prefersDark);
+  //   }
+  // );
 
-  const [isDark, setIsDark] = useState(systemPrefersDark);
+  const [isDark, setIsDark] = useState(false);
+  
+  useEffect(() => {
+    try {
+      console.log("checking dark ls")
+      const dark = localStorage.getItem('prevState')
+      console.log('prevState', dark)
+      console.log("typeof dark",typeof(dark))
+      if(dark != null) {
+        setIsDark(dark==='true')
+        if (dark) {
+          document.documentElement.classList.add(DARK_CLASS)
+        } else {
+          document.documentElement.classList.remove(DARK_CLASS)
+        }
+      }
+    }
+    catch(e) {
+
+    }
+  }, [])
 
      useEffect(() => {
+      console.log("changin dark",isDark)
        if (isDark) {
          document.documentElement.classList.add(DARK_CLASS)
        } else {
@@ -27,13 +48,18 @@ const DarkToggle = () => {
        }
      }, [isDark]);
 
+     console.log("isDark", isDark)
+
   return (
 
     <Toggle 
         className="DarkToggle"
-        onChange={event => setIsDark(event.target.checked)}
-        icons={{checked: <BsMoon color="white" />, unchecked: <BsSun color="yellow"/>}}
+        onChange={event => {
+          setIsDark(event.target.checked)
+          localStorage.setItem('prevState', event.target.checked)
+        }}
         checked={isDark}
+        icons={{checked: <BsMoon color="white" />, unchecked: <BsSun color="yellow"/>}}
         aria-label="Dark mode"
     />
   );
